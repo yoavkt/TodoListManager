94264c59688c3084fa80559549fb38f8050f8c66
yoavkt <yoavkt@gmail.com>
2013-03-12 11:37:51 +0200

    lay out commit 2

--- TodoListManager/gen/il/ac/huji/todolist/R.java
+++ TodoListManager/gen/il/ac/huji/todolist/R.java
@@ -27,11 +27,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int listView1=0x7f090000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int buttonAdd=0x7f090001;
+        public static final int itemAdd=0x7f090005;
+        public static final int listCourses=0x7f090002;
+        public static final int txtName=0x7f090003;
+        public static final int txtNekudot=0x7f090004;
     }
     public static final class layout {
         public static final int activity_todo_list_manager=0x7f030000;
+        public static final int row=0x7f030001;
     }
     public static final class menu {
         public static final int todo_list_manager=0x7f080000;
